1d651883c0965c7479af823125d4c8d72cacf160
root <root@Thorin>
2024-10-10 16:23:47 +0800

    server: add email and sql

--- server/src/main/java/com/pricehub/User.java
+++ server/src/main/java/com/pricehub/User.java
@@ -1,4 +1,4 @@
-package com.pricehub;
+package com.pricehub; 
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -9,6 +9,7 @@ public class User {
     private String id;
     private String name;
     private String password;
+    private String email; // 新增邮箱属性
 
     // Getters and Setters
     public String getId() {
@@ -34,4 +35,13 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getEmail() { // 新增邮箱的 Getter
+        return email;
+    }
+
+    public void setEmail(String email) { // 新增邮箱的 Setter
+        this.email = email;
+    }
 }
+
